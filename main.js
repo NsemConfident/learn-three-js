60d64fda06708fb7ed4423e232979eed0bf819a6
unknown <njocknsem@gmail.com>
2024-11-12 16:23:32 +0100

    feat: setup learning enviroment

---
+++ main.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+
+// 1. create a scene 
+
+// 2. Add a camera
+
+// 3. create and add a cube object
+
+// 4. Add lighting
+
+// 5. setup the render
+
+// 5. Animate the scene
